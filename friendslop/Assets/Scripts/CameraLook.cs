f70ff33ff9de59f6af063f2d8185ca5b5ca01f7b
HarryMonty <harrymonty@live.com.au>
2025-12-03 12:18:47 +1000

    CameraLook Script
    Created a simple script for the camera to look around during testing

---
+++ friendslop/Assets/Scripts/CameraLook.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public float mouseSensitivity = 200f;
+    private Transform cameraTransform;
+    float xRotation = 0f;
+    float yRotation = 0f;
+    void Start()
+    {
+        // Lock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        // Grab camera transform
+        cameraTransform = gameObject.transform;
+    }
+
+    void Update()
+    {
+        // Check for camera transform
+        if (!cameraTransform)
+        {
+            Debug.Log("No camera transform");
+            return;
+        }
+
+        // Grab user inputs
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        // Move camera based on user inputs and sensitivity
+        mouseX = mouseX * mouseSensitivity * Time.deltaTime;
+        mouseY = mouseY * mouseSensitivity * Time.deltaTime;
+
+        // Calculate horizontal & vertical movement
+        xRotation -= mouseY;
+        yRotation += mouseX;
+
+        xRotation = Mathf.Clamp(xRotation, -45f, 45f);
+        yRotation = Mathf.Clamp(yRotation, -45f, 45f);
+
+        // Change camera rotation
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+
+        Debug.Log("MouseX: " + mouseX + " MouseY: " + mouseY);
+    }
+}
